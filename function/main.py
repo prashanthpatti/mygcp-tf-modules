289bfc77efaed4a062ae91522baf0823685af20c
prashanthpatti <prashanthpatti@gmail.com>
2025-07-16 08:12:01 +0530

    added modules

--- function/main.py
+++
@@ -1,7 +0,0 @@
-from flask import jsonify
-
-def hello_world(request):
-    if request.method != 'GET':
-        return jsonify({"error": "Method not allowed"}), 405
-
-    return jsonify({"message": "Hello, World!"}), 200
--- tf-project/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "load_balancer_ip" {
-  value = module.load_balancer.lb_ip
-}
